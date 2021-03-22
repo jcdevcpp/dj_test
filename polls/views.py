0a3576982dbcd436b4369e619cc458e0e4b2cfdb
josec <jose.ramirez@enovate.ai>
2021-03-22 10:17:59 -0500

    Basic views and urls at polls app

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hola amigo Hello b8742743")
+
+def detail(request, question_id):
+    return HttpResponse(f"You are looking at question {question_id}")
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
