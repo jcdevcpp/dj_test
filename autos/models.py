9051090a726824a06392aa367b22b6651d9adedb
josec <jose.ramirez@enovate.ai>
2021-06-10 15:13:11 -0500

    Autos app debug1

--- autos/models.py
+++ autos/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from django.core.validators import MinlengthValidator
+from django.core.validators import MinLengthValidator
 
 
 class Auto(models.Model):
     nickname = models.CharField(
         max_length=200,
         help_text="Enter a nickname for your car (eg. Mcqueen)",
-        validators=[MinlengthValidator(2, "Nickname must be longer than 1 character")],
+        validators=[MinLengthValidator(2, "Nickname must be longer than 1 character")],
     )
     make = models.ForeignKey("Make", on_delete=models.CASCADE, null=False)
     mileage = models.PositiveIntegerField()
-    comments = models.Charfield(max_length=300)
+    comments = models.CharField(max_length=300)
 
     def __str__(self):
         return self.nickname
@@ -20,7 +20,7 @@ class Make(models.Model):
     name = models.CharField(
         max_length=200,
         help_text="Enter a make (eg. Dodge)",
-        validators=[MinlengthValidator(2, "Make must be greater thn 1 character")],
+        validators=[MinLengthValidator(2, "Make must be greater thn 1 character")],
     )
 
     def __str__(self):
