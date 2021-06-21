64c4de4ecff6fe40585b17bc58f4b9bace3d000e
josec <jose.ramirez@enovate.ai>
2021-06-21 17:10:56 -0500

    Cats assignment ready for autograder

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,17 +21,20 @@ from django.views.static import serve
 from django.views.generic import TemplateView
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SITE_ROOT = os.path.join(BASE_DIR, 'site')
+SITE_ROOT = os.path.join(BASE_DIR, "site")
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home/main.html')),
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-    path('hello/', include('hello.urls')),
-	path('accounts/', include('django.contrib.auth.urls')),
-	path('autos/', include('autos.urls')),
-    url(r'^site/(?P<path>.*)$', serve,
-        {'document_root': SITE_ROOT, 'show_indexes': True},
-        name='site_path'
-        ),
+    path("", TemplateView.as_view(template_name="home/main.html")),
+    path("admin/", admin.site.urls),
+    path("polls/", include("polls.urls")),
+    path("hello/", include("hello.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("autos/", include("autos.urls")),
+    path("cats/", include("cats.urls")),
+    url(
+        r"^site/(?P<path>.*)$",
+        serve,
+        {"document_root": SITE_ROOT, "show_indexes": True},
+        name="site_path",
+    ),
 ]
